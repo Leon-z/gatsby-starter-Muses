aacd106e3a15c02d28e28f7602661c2ebf002c4b
Leon <lz_n10828@163.com>
2018-05-06 23:08:58 +0800

    add sass cfg

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,26 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: 'Gatsby Starter Muses',
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-sass',
+    {
+    resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/post`,
+        name: "markdown-pages",
+      },
+    },
+    {
+    resolve: `gatsby-plugin-react-css-modules`,
+    options: {
+      filetypes: {
+        ".scss": { syntax: `postcss-scss` },
+      },
+      exclude: `\/global\/`,
+    },
+  },
+    'gatsby-transformer-remark'
+  ],
 }
--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,48 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+const path = require("path");
 
- // You can delete this file if you're not using it
+exports.createPages = ({ boundActionCreators, graphql }) => {
+  const { createPage } = boundActionCreators;
+
+  const blogPostTemplate = path.resolve(`src/templates/BlogPost.js`);
+
+  return graphql(`
+    {
+      allMarkdownRemark(
+        sort: { order: DESC, fields: [frontmatter___date] }
+        limit: 1000
+      ) {
+        edges {
+          node {
+            frontmatter {
+              path
+            }
+          }
+        }
+      }
+    }
+  `).then(result => {
+    if (result.errors) {
+      return Promise.reject(result.errors);
+    }
+
+    result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+      createPage({
+        path: node.frontmatter.path,
+        component: blogPostTemplate,
+        context: {}, // additional data can be passed via context
+      });
+    });
+  });
+};
+
+/**
+ * remove third party window rely on
+ */
+exports.modifyWebpackConfig = ({ config, stage }) => {
+  if (stage === "build-html") {
+    config.loader("null", {
+      test: /bad-module/,
+      loader: "null-loader",
+    });
+  }
+};
